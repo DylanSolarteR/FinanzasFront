bd065639183c7f40342dd3df6b288554bad6a20f
Michael Jimenez <72152481+Michael-Jimenez-C@users.noreply.github.com>
2024-04-11 18:36:07 -0500

    sidenavbar en proceso

--- src/components/navside.jsx
+++ src/components/navside.jsx
@@ -1,8 +1,12 @@
+import { data } from "autoprefixer"
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
-function NavButton(){
+function NavButton({text,href,imgsrc, state}){
     return(
-        <></>
+        <Link className='px-8 py-4 text-slate-700 hover:text-slate-200 hover:bg-slate-800 rounded transition-colors duration-300 flex justify-between' to={href}>
+            <img src={imgsrc} className='p-2 rounded-full bg-slate-200'/>{text}
+        </Link>
     )
 }
 
@@ -12,6 +16,11 @@ export default function NavSide(){
         setstate(state==0? 1: 0)
     }
 
+    let navigation = [
+        ['/','/tag.svg','Etiquetas'],
+        ['/','/dollar-sign.svg','Finanzas']
+    ]
+
     return(
     <>
     <div className={"bg-slate-200 "+(state==1? "w-64":"w-12")}>
@@ -19,11 +28,12 @@ export default function NavSide(){
                 ()=>{cambiar_estado(state)}
                 }>
         <img src={state==1? "/x.svg":"/menu.svg"} className='px-2 invert h-full'/>
-        <div className="text-center mx-auto text-xl">
+        <div className="text-slate-700 text-center mx-auto text-xl">
             {state==1? "Menu":""}
         </div>
     </button>
+    {navigation.map((dato)=>(<NavButton text={state==1? dato[2]:""} state={state} href={dato[0]} imgsrc={dato[1]}/>))}
     </div>
     </>
     )
-}
+}//{navigation.map((dato)=>(NavButton(dato[0],dato[1],dato[2])))}
