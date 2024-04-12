c2cc56665bef5d1352d0913c951748392a34ac8e
Michael Jimenez <72152481+Michael-Jimenez-C@users.noreply.github.com>
2024-04-11 19:23:48 -0500

    animación en la sidebar

--- src/components/navside.jsx
+++ src/components/navside.jsx
@@ -2,10 +2,14 @@ import { data } from "autoprefixer"
 import { useState } from "react"
 import { Link } from "react-router-dom"
 
-function NavButton({text,href,imgsrc, state}){
+
+function NavButton({text,href,imgsrc}){
     return(
-        <Link className='px-8 py-4 text-slate-700 hover:text-slate-200 hover:bg-slate-800 rounded transition-colors duration-300 flex justify-between' to={href}>
-            <img src={imgsrc} className='p-2 rounded-full bg-slate-200'/>{text}
+        <Link className='py-4 text-slate-700 hover:bg-slate-800 group rounded transition-colors duration-300 flex' to={href}>
+            <img src={imgsrc} className={'mx-1 p-2 rounded-full bg-slate-200'}/>
+            <div className="text-slate-700 group-hover:text-slate-200 text-center mx-auto text-xl">
+            {text}
+            </div>
         </Link>
     )
 }
@@ -23,17 +27,17 @@ export default function NavSide(){
 
     return(
     <>
-    <div className={"bg-slate-200 "+(state==1? "w-64":"w-12")}>
+    <div className={"bg-slate-200 duration-300 transition-all " +(state==1? "animation-expand w-64":"w-12")}>
     <button className='bg-slate-300 transition-colors duration-300 inline-flex w-full h-10 hover:bg-slate-400 items-center ' onClick={
                 ()=>{cambiar_estado(state)}
                 }>
-        <img src={state==1? "/x.svg":"/menu.svg"} className='px-2 invert h-full'/>
-        <div className="text-slate-700 text-center mx-auto text-xl">
+        <img src={state==1? "/x.svg":"/menu.svg"} className='fill-slate-100 text-slate-100 px-2 h-full'/>
+        <div className={"text-slate-700 text-center mx-auto text-xl"}>
             {state==1? "Menu":""}
         </div>
     </button>
-    {navigation.map((dato)=>(<NavButton text={state==1? dato[2]:""} state={state} href={dato[0]} imgsrc={dato[1]}/>))}
+    {navigation.map((dato)=>(<NavButton text={state==1? dato[2]:""} href={dato[0]} imgsrc={dato[1]}/>))}
     </div>
     </>
     )
-}//{navigation.map((dato)=>(NavButton(dato[0],dato[1],dato[2])))}
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        expand: {
+          "0%": { width: "15rem" },
+          "100%": { width: "15rem" },
+        },
+        appear: {
+          "0%": { opacity: '0' },
+          "90%": { opacity: '0' },
+          "100%": { opacity: '100%' },
+        },
+      },
+    },
   },
   plugins: ["prettier-plugin-tailwindcss"],
 };
