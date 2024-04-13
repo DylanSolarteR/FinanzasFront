e2364accf2a5349bb20b1fd5c6d329b7f51841a6
DylanSolarteR <dasolarter@udistrital.edu.co>
2024-04-13 11:56:06 -0500

    funcionalidad register

--- src/fetchs/fetchRegister.js
+++ src/fetchs/fetchRegister.js
@@ -1,27 +1,26 @@
-import { RutasAPI } from '../assets/RutasAPI';
-export default async function fetchRegister(name, correo, pw, auth) {
+import { RutasAPI } from "../assets/RutasAPI";
+export default async function fetchRegister(name, correo, pw) {
   try {
-    // Realiza la solicitud a la API con la autorización en el encabezado
+    // Realiza la solicitud a la API
     const response = await fetch(RutasAPI.AddUsuario, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: auth
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name: name,
         email: correo,
-        pss: pw
-      })
+        pss: pw,
+      }),
     });
 
     if (response.ok) {
       return 1;
     } else {
-      console.error('Error en el inicio de sesión');
+      console.error("Error en el inicio de sesión");
       return 0;
     }
   } catch (error) {
-    console.error('Error en la solicitud:', error);
+    console.error("Error en la solicitud:", error);
   }
 }
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,72 +1,82 @@
-import React, { useState } from 'react';
-import {Link, useNavigate} from 'react-router-dom'
-import fetchRegister from '../fetchs/fetchRegister';
-import fetchUsers from '../fetchs/fetchUsers'
-import SignLayOut from '../components/forms/sign-layout';
-import SubmitButton from '../components/forms/SubmitButton';
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import fetchRegister from "../fetchs/fetchRegister";
+import fetchUsers from "../fetchs/fetchUsers";
+import SignLayOut from "../components/forms/sign-layout";
+import SubmitButton from "../components/forms/SubmitButton";
+import { useGlobalContext } from "../context/globalContext";
 
 function Register() {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const auth = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTYxNTk4YWY3YTNmNWQ2MDY0NDY3NDUiLCJpYXQiOjE3MDA5MjQxNDUsImV4cCI6MTcwMTAxMDU0NX0.qB94xrxR_yoijcb4ycIXNRhMFAxoQawDLx1iGIp-i50";
+  const { setIsLogged, setUser, setToken, setUserFinanzas, setUserTags } =
+    useGlobalContext();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   async function getUser() {
-    const fetchedUsers = await fetchUsers(auth);
-    const usuario = fetchedUsers.usuarios.find(u => u.email==email);
+    const fetchedUsers = await fetchUsers();
+    const usuario = fetchedUsers.usuarios.find((u) => u.email == email);
     return usuario;
-}
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetchRegister(username, email, password, auth);
-    if(response){
-        const user = await getUser()
-        navigate('/dashboard', { state: {user: user} });
-      
+    const response = await fetchRegister(username, email, password);
+    if (response) {
+      const user = await getUser();
+      setUser(user);
+      setIsLogged(true);
+      setToken(response.token);
+      setUserFinanzas(user.finanzas);
+      setUserTags(user.tags);
+      navigate("/dashboard");
     }
   };
 
   return (
     <SignLayOut>
-        <h1 className="text-2xl font-bold mb-6">Registro</h1>
-        <form className='grid' onSubmit={handleSubmit}>
-          <label className="mb-4">
-            <span className="text-gray-700">Nombre de Usuario:</span>
-            <input
-              type="text"
-              value={username}
-              placeholder='Ej: Pedro'
-              onChange={(e) => setUsername(e.target.value)}
-              className="mt-1  w-full rounded-md p-1 focus:outline-slate-400 border-black border-2 placeholder:italic placeholder:text-slate-400"
-            />
-          </label>
-          <label className="mb-4">
-            <span className="text-gray-700">Correo Electrónico:</span>
-            <input
-              type="email"
-              value={email}
-              placeholder='Ej: hola@example.com'
-              onChange={(e) => setEmail(e.target.value)}
-              className="mt-1 w-full rounded-md p-1 focus:outline-slate-400 border-black border-2 placeholder:italic placeholder:text-slate-400"
-            />
-          </label>
-          <label className="mb-4">
-            <span className="text-gray-700">Contraseña:</span>
-            <input
-              type="password"
-              value={password}
-              placeholder='Ej: 12345'
-              onChange={(e) => setPassword(e.target.value)}
-              className="mt-1 block w-full rounded-md p-1 focus:outline-slate-400 border-black border-2 placeholder:italic placeholder:text-slate-400"
-            />
-          </label>
-          <SubmitButton>Registrarse</SubmitButton>
-          <p className='text-gray-700 text-center'>¿Ya tienes cuenta? <Link to='/login' className='font-bold'>Inicia Sesion</Link></p>
-        </form>
+      <h1 className="text-2xl font-bold mb-6">Registro</h1>
+      <form className="grid" onSubmit={handleSubmit}>
+        <label className="mb-4">
+          <span className="text-gray-700">Nombre de Usuario:</span>
+          <input
+            type="text"
+            value={username}
+            placeholder="Ej: Pedro"
+            onChange={(e) => setUsername(e.target.value)}
+            className="mt-1  w-full rounded-md p-1 focus:outline-slate-400 border-black border-2 placeholder:italic placeholder:text-slate-400"
+          />
+        </label>
+        <label className="mb-4">
+          <span className="text-gray-700">Correo Electrónico:</span>
+          <input
+            type="email"
+            value={email}
+            placeholder="Ej: hola@example.com"
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 w-full rounded-md p-1 focus:outline-slate-400 border-black border-2 placeholder:italic placeholder:text-slate-400"
+          />
+        </label>
+        <label className="mb-4">
+          <span className="text-gray-700">Contraseña:</span>
+          <input
+            type="password"
+            value={password}
+            placeholder="Ej: 12345"
+            onChange={(e) => setPassword(e.target.value)}
+            className="mt-1 block w-full rounded-md p-1 focus:outline-slate-400 border-black border-2 placeholder:italic placeholder:text-slate-400"
+          />
+        </label>
+        <SubmitButton>Registrarse</SubmitButton>
+        <p className="text-gray-700 text-center">
+          ¿Ya tienes cuenta?{" "}
+          <Link to="/login" className="font-bold">
+            Inicia Sesion
+          </Link>
+        </p>
+      </form>
     </SignLayOut>
   );
 }
